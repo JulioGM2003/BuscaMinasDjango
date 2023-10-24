2a67c806b63b2a728dc9bfc61b877a0944387d8a
JulioGM2003 <jgarmol515@iesmartinezm.es>
2023-10-24 20:48:56 +0200

    Pequeños avances

--- buscaMinasForm/views.py
+++ buscaMinasForm/views.py
@@ -1,9 +1,19 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
+from .forms import TableroForm
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def crea_tablero(request):
-    return render(request, 'crea_tablero.html')
+    if request.method == 'POST':
+        form = TableroForm(request.POST)
+        if form.is_valid():
+            filas = form.cleaned_data['filas']
+            columnas = form.cleaned_data['columnas']
+            return render(request, 'crea_tablero.html', {
+                'filas': filas,
+                'columnas': columnas
+            })
+    else:
+        form = TableroForm()
+    return render(request, 'crea_tablero.html', {'form': form})
